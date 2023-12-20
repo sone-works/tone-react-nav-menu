9b237eee57759f106a495690564aef6fee9a1b89
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-20 18:03:56 -0500

    feat: loggedIn switch

--- src/NavMenu.tsx
+++ src/NavMenu.tsx
@@ -19,7 +19,7 @@ export default function NavMenu({ useUserStore }: NavMenuProps) {
 
   const isHidden = hiddenPaths.includes(pathname)
 
-  return !isHidden ? (
+  return !user.isLoggedIn || !isHidden ? (
     <div className="w-full bg-global text-global flex items-center justify-between p-2 sticky top-0 border-b-2 border-global">
       <span className="text-4xl font-release font-bold">tone</span>
       <DarkModeSwitch />
