d7c7afc8a1c655e180d57b0282145c135bb23cab
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-27 15:55:00 -0500

    fix: hydration error

--- src/NavMenu.tsx
+++ src/NavMenu.tsx
@@ -1,44 +1,46 @@
-import useUserStore from '@sone-dao/tone-react-user-store'
+import { ToneUser } from '@sone-dao/tone-react-user-store'
 import { usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import DarkModeSwitch from './components/DarkModeSwitch'
 import HamburgerButton from './components/HamburgerButton'
 import MegaMenu from './components/MegaMenu'
 
-type NavMenuProps = {}
+type NavMenuProps = {
+  user: ToneUser
+}
 
-export default function NavMenu({}: NavMenuProps) {
-  const [isLoaded, setLoaded] = useState<boolean>(false)
+const toneUserDefaults = {
+  isLoggedIn: false,
+  userId: '',
+  display: '',
+  description: '',
+  colors: ['', ''],
+  socials: {},
+  location: '',
+  avatar: {
+    dataURL: '',
+  },
+}
+
+export default function NavMenu({ user }: NavMenuProps) {
   const [isMegaOpen, setMegaOpen] = useState<boolean>(false)
 
   const pathname = usePathname()
-  const user = useUserStore()
 
   const hiddenPaths = ['/signup', '/login']
 
   const isHidden = hiddenPaths.includes(pathname)
 
-  useEffect(() => {
-    setLoaded(true)
-  }, [])
+  if (!user) user = toneUserDefaults
 
   return user.isLoggedIn && !isHidden ? (
-    <div
-      className="
-        w-full bg-global text-global flex items-center justify-between p-2 sticky top-0 border-b-2 border-global"
-    >
+    <div className="w-full bg-global text-global flex items-center justify-between p-2 sticky top-0 border-b-2 border-global">
       <span className="text-4xl font-release font-bold">tone</span>
       <DarkModeSwitch />
       <HamburgerButton onClick={() => setMegaOpen(!isMegaOpen)} />
-      {isLoaded && (
-        <MegaMenu
-          isMegaOpen={isMegaOpen}
-          setMegaOpen={setMegaOpen}
-          user={user}
-        />
-      )}
+      <MegaMenu isMegaOpen={isMegaOpen} setMegaOpen={setMegaOpen} user={user} />
     </div>
   ) : (
-    <div className="hidden" />
+    <></>
   )
 }
